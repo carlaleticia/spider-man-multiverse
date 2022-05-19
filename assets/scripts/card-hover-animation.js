e15eb114d1c6abf503c8d5cc786ce52facc1bbd6
Carla <carlaleticia.gf@gmail.com>
2022-05-19 18:53:27 -0300

    updates second live

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -19,3 +19,18 @@ function addEventListenerToCards() {
 }
 
 document.addEventListener('DOMContentLoaded', addEventListenerToCards, false);
+
+function selectCarouselItem(selectedButtonElement) {
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.s-cards-carousel');
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+  
+    carousel.style.transform = newTransform;
+  
+    const activeButtonElement = document.querySelector('.s-controller__button--active');
+    activeButtonElement.classList.remove('s-controller__button--active');
+    selectedButtonElement.classList.add('s-controller__button--active');
+  }
